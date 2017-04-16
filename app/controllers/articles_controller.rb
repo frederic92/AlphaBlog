317a70d6cddd73f5b4abf3764021b349507e3b2c
frederic <fredericmbea@gmail.com>
2017-04-16 18:09:44 -0400

    Added before action for show, edit, update and destroy actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def new
     @article = Article.new
   end
@@ -13,20 +14,17 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def show
-    @article = Article.find(params[:id])
+  def show 
   end
 
   def index
     @articles = Article.all
   end
 
-  def edit
-    @article = Article.find(params[:id])
+  def edit 
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -36,13 +34,15 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully destroyed"
     redirect_to articles_path
   end
 
   private
+    def set_article
+      @article = Article.find(params[:id])
+    end
     def article_params
       params.require(:article).permit(:title, :description)
     end
