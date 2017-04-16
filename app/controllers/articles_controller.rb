fd0e9895783074e4b0c48617c282a7f6e9169091
frederic <fredericmbea@gmail.com>
2017-04-16 03:05:03 -0400

    Added flash and error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,11 +5,16 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to article_path(@article)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render "new"
+    end
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,4 @@
 Rails.application.routes.draw do
-  get 'articles/new'
-
-  get 'articles/create'
-
-  get 'articles/index'
-
-  get 'articles/edit'
-
-  get 'articles/update'
-
-  get 'articles/destroy'
-
   get '/', to: 'pages#home'
 
   get 'pages/about'
