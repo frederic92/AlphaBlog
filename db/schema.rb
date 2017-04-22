a96ca7d57e3d2ce9633ff196e5f9ee74b8cd286b
frederic <fredericmbea@gmail.com>
2017-04-21 22:10:17 -0400

    Added secure password and attribute for users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421221904) do
+ActiveRecord::Schema.define(version: 20170422020552) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -31,8 +31,9 @@ ActiveRecord::Schema.define(version: 20170421221904) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
